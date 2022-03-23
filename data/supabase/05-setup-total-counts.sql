9a8cc5c24aae5a2d2be053a40e57de51cd25d0e0
A. David Thyresson <dthyresson@gmail.com>
2022-03-22 20:37:40 -0400

    Just count on Vote not Up or Down models

--- data/supabase/05-setup-total-counts.sql
+++ data/supabase/05-setup-total-counts.sql
@@ -1,6 +1,5 @@
-comment on table "UpVote" is '@graphql({"totalCount": {"enabled": true}})';
-comment on table "DownVote" is '@graphql({"totalCount": {"enabled": true}})';
+comment on table "Profile" is '@graphql({"totalCount": {"enabled": true}})';
 comment on table "Post" is '@graphql({"totalCount": {"enabled": true}})';
 comment on table "Comment" is '@graphql({"totalCount": {"enabled": true}})';
-comment on table "Profile" is '@graphql({"totalCount": {"enabled": true}})';
+comment on table "Vote" is '@graphql({"totalCount": {"enabled": true}})';
 

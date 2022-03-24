f755bf5b7ea732796be28a5b4ea206df0db5ad41
Laurin Quast <laurinquast@googlemail.com>
2022-03-24 13:38:35 +0100

    refactor: move items to separate files

--- app/lib/feed-item.tsx
+++ app/lib/feed-item.tsx
@@ -1,13 +1,9 @@
 import Link from "next/link";
 import React from "react";
-import TimeAgo from "javascript-time-ago";
-import en from "javascript-time-ago/locale/en.json";
 
 import { DocumentType, gql } from "../gql";
-
-TimeAgo.addDefaultLocale(en);
-
-const timeAgo = new TimeAgo("en-US");
+import { CalendarIcon, CommentIcon, PointIcon } from "./icons";
+import { timeAgo } from "./time-ago";
 
 const FeedItem_PostFragment = gql(/* GraphQL */ `
   fragment FeedItem_PostFragment on Post {
@@ -25,57 +21,6 @@ const FeedItem_PostFragment = gql(/* GraphQL */ `
     }
   }
 `);
-
-function CalendarIcon(props: { className?: string }) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth={2}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <rect x={3} y={4} width={18} height={18} rx={2} ry={2} />
-      <path d="M16 2v4M8 2v4M3 10h18" />
-    </svg>
-  );
-}
-
-function CommentIcon(props: { className?: string }) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      stroke-width="2"
-      fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      {...props}
-    >
-      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-    </svg>
-  );
-}
-
-function PointIcon(props: { className?: string }) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      className="w-4 h-4 mr-1"
-      stroke="currentColor"
-      stroke-width="2"
-      fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      {...props}
-    >
-      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-      <circle cx="12" cy="12" r="3"></circle>
-    </svg>
-  );
-}
 
 export function FeedItem(props: {
   post: DocumentType<typeof FeedItem_PostFragment>;

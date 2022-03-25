39bf4c8a45827f09f7b59d804c1b98ef9a904f19
Laurin Quast <laurinquast@googlemail.com>
2022-03-25 09:16:11 +0100

    feat: lets not center everything

--- app/lib/main-section.tsx
+++ app/lib/main-section.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function MainSection(props: { children: React.ReactNode }) {
   return (
-    <main className="min-h-screen px-4 py-0 flex-1 flex flex-column justify-center items-center">
+    <main className="min-h-screen px-4 py-0 flex-1 flex flex-column justify-center">
       {props.children}
     </main>
   );

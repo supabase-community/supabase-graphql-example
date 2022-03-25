9e172f5b154a2f1e3bcb132ffc8447cb4b98252e
A. David Thyresson <dthyresson@gmail.com>
2022-03-24 22:52:05 -0400

    Updates handle_new_user

--- data/supabase/05-setup-user-profile-trigger.sql
+++ data/supabase/05-setup-user-profile-trigger.sql
@@ -1,68 +1,13 @@
-CREATE OR REPLACE FUNCTION public.update_vote_counts ()
-	RETURNS TRIGGER
-	AS $$
-BEGIN
-	WITH u AS (
-		SELECT
-			v. "postId",
-			coalesce(count(*),
-				0) AS total
-		FROM
-			public. "Vote" v
-		WHERE
-			v.direction = 'UP'
-		GROUP BY
-			v. "postId"
-),
-d AS (
-	SELECT
-		v. "postId",
-		coalesce(count(*),
-			0) AS total
-	FROM
-		public. "Vote" v
-	WHERE
-		v.direction = 'DOWN'
-	GROUP BY
-		v. "postId"
-),
-t AS (
-	SELECT
-		p.id,
-		coalesce(u.total,
-			0) AS "upVoteTotal",
-		coalesce(d.total,
-			0) AS "downVoteTotal",
-		coalesce(u.total,
-			0) - coalesce(d.total,
-			0) AS "voteTotal"
-	FROM
-		public. "Post" p
-	LEFT JOIN u ON p.id = u. "postId"
-	LEFT JOIN d ON p.id = d. "postId"
-),
-r AS (
-	SELECT
-		*,
-		DENSE_RANK() OVER (ORDER BY t. "voteTotal" DESC) AS "voteRank"
-	FROM
-		t
-)
-UPDATE
-	public. "Post"
-SET
-	"upVoteTotal" = r. "upVoteTotal",
-	"downVoteTotal" = r. "downVoteTotal",
-	"voteTotal" = r. "voteTotal",
-	"voteRank" = r. "voteRank"
-FROM
-	r
-WHERE
-	r.id = public. "Post".id;
-  RETURN new;
-END;
-$$ LANGUAGE plpgsql SECURITY DEFINER;
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+begin
+  insert into public."Profile" (id, "avatarUrl", username)
+  values (new.id, new.raw_user_meta_data->>'avatar_url', split_part(new.email, '@', 1) || '-' || floor(random() * 10000));
+  return new;
+end;
+$function$
 
-CREATE TRIGGER on_vote_created AFTER INSERT ON public."Vote" FOR EACH ROW EXECUTE FUNCTION public.update_vote_counts();
-
-CREATE  TRIGGER on_vote_deleted AFTER DELETE ON public."Vote" FOR EACH ROW EXECUTE FUNCTION public.update_vote_counts();
+CREATE TRIGGER on_auth_user_created AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION public.handle_new_user();

a1155fc9f1584c4b8b51f4e19ec0c2fea9c3706e
Laurin Quast <laurinquast@googlemail.com>
2022-03-25 09:19:44 +0100

    feat: add supabase icon

--- app/lib/navigation.tsx
+++ app/lib/navigation.tsx
@@ -1,6 +1,7 @@
 import { Auth } from "@supabase/ui";
 import Link from "next/link";
 import React from "react";
+import { SupabaseIcon } from "./icons";
 
 export function Navigation() {
   const user = Auth.useUser();
@@ -9,18 +10,7 @@ export function Navigation() {
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <Link href="/">
           <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
+            <SupabaseIcon height={24} />
             <span className="ml-3 text-xl">supanews</span>
           </a>
         </Link>

de1cd62bb08bb9176f74726387c882ab10963005
Laurin Quast <laurinquast@googlemail.com>
2022-03-25 12:45:33 +0100

    feat: login button style refactor

--- app/lib/navigation.tsx
+++ app/lib/navigation.tsx
@@ -30,21 +30,10 @@ export function Navigation() {
           </ActiveLink>
         </nav>
         {user.user === null ? (
-          <Link href="login">
-            <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+          <Link href="/login">
+            <a className="inline-flex items-center mt-4 md:mt-0 md:mr-5 text-gray-400 hover:text-gray-900">
               login
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="w-4 h-4 ml-1"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5 12h14M12 5l7 7-7 7"></path>
-              </svg>
-            </button>
+            </a>
           </Link>
         ) : (
           <>
